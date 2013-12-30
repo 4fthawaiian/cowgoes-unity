163b72ad0e0252ec00f16f0e86c0ea0352eda620
bradley mccrorey <bradley.mccrorey@projectpixelpress.com>
2013-12-30 14:18:18 -0600

    dynamic web views working

--- Assets/execution_order.js
+++ Assets/execution_order.js
@@ -3,29 +3,31 @@ import MiniJSON;
 import System.Collections.Generic;
 
 var web_viewer : GameObject;
-function get_animals() {
-	var www : WWW = new WWW ("http://vpn.mccroreys.com:3000/v1/animals");
+
+function Start()
+{
+	var viewport_right = 3; // outside right position to start at creating web views
+  var counter=0;
+	var web_animals : WWW = new WWW ("http://vpn.mccroreys.com:3000/v1/animals");
 	// Wait for download to complete
-	yield www;
-	var animals = www.text;
+	yield web_animals;
+	var animals = web_animals.text;
 	var dict = Json.Deserialize(animals) as Dictionary.<String,System.Object>;
 	for(var key in dict) {
 		Debug.Log(key.Key+" is the key!");
-		var thisval = key.Value as Dictionary.<String,System.Object>;
-		for(var valkey in thisval) {
-			Debug.Log(valkey.Key);
-		}
-	}
-}
-function Start()
-{
-	get_animals();
-	var total_viewers = 5;
-	var viewport_right = 3; // outside right position to start at creating web views
-	for(var i=0;i<total_viewers;i++) {
-	    var x = (viewport_right + (0 - (.75*i) ));
+		// var thisval = key.Value as Dictionary.<String,System.Object>;
+		// for(var valkey in thisval) {
+		// 	Debug.Log(valkey.Key);
+		// }
+    var this_animal = key.Key;
+    var x = (viewport_right + (0 - (.75*counter) ));
 		var animal_view = Instantiate(web_viewer, Vector3(x,1,5), Quaternion.identity);
-		Debug.Log(animal_view);
+    Debug.Log(animal_view);
+    var m_View = animal_view.GetComponentInChildren(CoherentUIView);
+    Debug.Log("http://localhost:3000/animal/"+this_animal);
+    Debug.Log(m_View);
+    m_View.Page = "http://localhost:3000/animal/"+this_animal;
+    counter++;
 	}
 }
 
