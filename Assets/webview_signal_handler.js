54c4256941c80dc76a9d883e65b743d877fdaa96
bradley mccrorey <bradley.mccrorey@projectpixelpress.com>
2013-12-30 14:18:41 -0600

    dynamic web views working

---
+++ Assets/webview_signal_handler.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+var web_viewer : GameObject;
+
+function Start () {
+  var m_View = GetComponent(CoherentUIView);
+  Debug.Log("MVIEW: "+m_View);
+  Debug.Log(m_View.Page);
+  m_View.Listener.ReadyForBindings += function(frameId, path, isMainFrame) {
+  if (isMainFrame)
+    {
+      m_View.View.BindCall("accept_call", this.AcceptCall);
+      Debug.Log("PATHPATH"+path);
+    }
+  };
+}
+
+function AcceptCall() {
+  Debug.Log("!!AcceptCall!!");
+}
+
+function Update () {
+
+}
